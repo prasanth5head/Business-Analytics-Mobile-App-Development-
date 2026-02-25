897d00ae65818fef19cf2573f20d931502b46486
Prasanth <prasanthpower5p@gmail.com>
2026-02-25 10:11:23 +0530

    New Version category colors

--- frontend/src/pages/Descriptive.js
+++ frontend/src/pages/Descriptive.js
@@ -15,10 +15,12 @@ const Descriptive = () => {
     const { marketData, aiRecommendations, loading, error, refreshData } = useMarket();
 
     const COLORS = [
-        theme.palette.primary.main,
-        theme.palette.secondary.main,
-        theme.palette.error.main,
-        theme.palette.success.main
+        '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399'
     ];
 
     if (loading && !marketData) {
